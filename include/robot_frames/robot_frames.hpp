afdc9e8dc6a67614d15101bf674c9796b96c9ed6
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-18 01:36:06 +0700

    feat: main hpp

---
+++ include/robot_frames/robot_frames.hpp
@@ -0,0 +1,7 @@
+#ifndef ROBOT_FRAMES__HPP_
+#define ROBOT_FRAMES__HPP_
+
+#include "robot_frames/model/robot_wrapper.hpp"
+#include "robot_frames/node/robot_frames_node.hpp"
+
+#endif  // ROBOT_FRAMES__HPP_
