76d5ad8a5cb8912740e40731612009f5d9a5ac84
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-18 01:35:58 +0700

    feat: add ros2 node

---
+++ include/robot_frames/node/robot_frames_node.hpp
@@ -0,0 +1,48 @@
+#ifndef ROBOT_FRAMES__NODE__ROBOT_FRAMES_NODE_HPP_
+#define ROBOT_FRAMES__NODE__ROBOT_FRAMES_NODE_HPP_
+
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+
+#include "kansei_interfaces/msg/status.hpp"
+#include "robot_frames/model/robot_wrapper.hpp"
+#include "tachimawari_interfaces/msg/current_joints.hpp"
+
+namespace robot_frames
+{
+class RobotFramesNode
+{
+public:
+  using CurrentJoints = tachimawari_interfaces::msg::CurrentJoints;
+  using KanseiStatus = kansei_interfaces::msg::Status;
+  using TransformStamped = geometry_msgs::msg::TransformStamped;
+
+  RobotFramesNode(const rclcpp::Node::SharedPtr & node, const std::string & urdf_path);
+
+  void publish_frames(
+    const std::map<std::string, double> & joint_positions, const rclcpp::Time & time);
+  void update_joints(const CurrentJoints::SharedPtr msg);
+  void publish_static_frames();
+
+private:
+  rclcpp::Node::SharedPtr node;
+  rclcpp::TimerBase::SharedPtr node_timer;
+  rclcpp::Time last_publish_time;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster;
+
+  rclcpp::Subscription<CurrentJoints>::SharedPtr current_joints_subscriber;
+
+  rclcpp::Subscription<KanseiStatus>::SharedPtr kansei_status_subscriber;
+
+  std::shared_ptr<RobotWrapper> robot_wrapper;
+};
+
+}  // namespace robot_frames
+
+#endif  // ROBOT_FRAMES__NODE__ROBOT_FRAMES_NODE_HPP_
